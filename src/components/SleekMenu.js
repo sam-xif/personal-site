54722629c34dbdd023c1336f5e530eeee749576f
sam-xif <s.xifaras999@gmail.com>
2020-04-11 09:03:57 -0400

    Fixed some styling and added fade out on page navigation

--- src/components/SleekMenu.js
+++ src/components/SleekMenu.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     '&:hover, &$focusVisible': {
       zIndex: 1,
       '& $imageBackdrop': {
-        opacity: 0.8,
+        opacity: 0.9,
       },
       '& $imageMarked': {
         opacity: 0,
@@ -64,18 +64,20 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.common.white,
   },
   imageBackdrop: {
+    zIndex: -2,
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.9,
+    backgroundColor: theme.palette.primary.dark,
+    opacity: 1.0,
     transition: theme.transitions.create('opacity', { duration: '0.5s' }),
   },
   imageTitle: {
     position: 'relative',
     padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+    bottom: 30
   },
   imageMarked: {
     height: 3,
@@ -90,6 +92,8 @@ const useStyles = makeStyles(theme => ({
 
 export default () => {
   const classes = useStyles()
+  const [fadingOut, setFadingOut] = useState(false)
+  const navigateDelay = 800
   return (
     <>
       <Grid container style={{height: '100%'}}>
@@ -102,10 +106,13 @@ export default () => {
               width: '100%',
               height: '100%',
             }}
-            onClick={() => setTimeout(() => navigate('/blog'), 300)}
+            onClick={() => {
+              setFadingOut(true)
+              setTimeout(() => navigate('/blog'), navigateDelay)
+            }}
           >
             <span className={classes.imageBackdrop}/>
-            <Fade in={true} timeout={800}>
+            <Fade in={!fadingOut} timeout={800}>
               <span className={classes.imageButton}>
                 <Typography
                   component="span"
@@ -129,7 +136,10 @@ export default () => {
               width: '100%',
               height: '100%',
             }}
-            onClick={() => setTimeout(() => navigate('/portfolio'), 300)}
+            onClick={() => {
+              setFadingOut(true)
+              setTimeout(() => navigate('/portfolio'), navigateDelay)
+            }}
           >
             {/*<span*/}
             {/*  className={classes.imageSrc}*/}
@@ -139,7 +149,7 @@ export default () => {
             {/*  }}*/}
             {/*/>*/}
             <span className={classes.imageBackdrop}/>
-            <Fade in={true} timeout={1600}>
+            <Fade in={!fadingOut} timeout={1600}>
               <span className={classes.imageButton}>
                 <Typography
                   component="span"
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,25 @@ import React from "react"
 import { theme } from "../constants/getTheme"
 import Header from "../components/Header"
 import ThemeProvider from "@material-ui/styles/ThemeProvider"
+import makeStyles from "@material-ui/core/styles/makeStyles"
+
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    height: '100%',
+    backgroundColor: theme.palette.primary.dark
+  }
+}))
 
 export default () => {
   return(<ThemeProvider theme={theme}>
-    <div style={{height: '100%', overflow: 'hidden'}}>
-      <Header />
-    </div>
+    <BlogContent />
   </ThemeProvider>)
+}
+
+const BlogContent = () => {
+  const classes = useStyles()
+
+  return (<div className={classes.wrapper}>
+    <Header />
+  </div>)
 }
